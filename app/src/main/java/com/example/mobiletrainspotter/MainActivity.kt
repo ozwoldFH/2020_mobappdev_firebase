0c792c4ae5c74dc42942ed7ff0debe1f2ea1e11c
ShelbyBoss <42971547+ShelbyBoss@users.noreply.github.com>
2020-05-29 21:51:42 +0200

    removed test data

--- app/src/main/java/com/example/mobiletrainspotter/MainActivity.kt
+++ app/src/main/java/com/example/mobiletrainspotter/MainActivity.kt
@@ -140,62 +140,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun loadData() {
-        val trainList = arrayListOf<Train>(
-            Train(
-                arrayListOf("https://www.oebb.at/thumbnails/www.nightjet.com/.imaging/default/dam/reiseportal/bildergalerie-2560x1600/cityjet-eco/eco14.jpg/jcr:content.jpg?t=1574245465322&scale=0.5"),
-                arrayListOf(TrainPart("4746", "049")),
-                "Österreich",
-                "S398?",
-                "A great train",
-                LocalDateTime.parse("2019-06-29T14:03:18")
-            ),
-            Train(
-                arrayListOf("https://c1.staticflickr.com/1/646/32395062573_83d3a6272a_b.jpg"),
-                arrayListOf(
-                    TrainPart("1144", "40"),
-                    TrainPart("21-73", "122"),
-                    TrainPart("21-73", "405"),
-                    TrainPart("21-73", "298"),
-                    TrainPart("80-73", "201")
-                ),
-                "Österreich #2",
-                "Güterzug",
-                "A legend",
-                LocalDateTime.now()
-            ),
-            Train(
-                arrayListOf("https://upload.wikimedia.org/wikipedia/commons/8/83/Oesterreich_euro2008lok.jpg"),
-                arrayListOf(TrainPart("1116", "005")),
-                "Österreich #3",
-                "",
-                "The instrument",
-                LocalDateTime.parse("2019-08-03T09:51:39")
-            ),
-            Train(
-                arrayListOf(),
-                arrayListOf(TrainPart("5022", "51")),
-                "Österreich #3",
-                "",
-                "The instrument",
-                LocalDateTime.parse("2020-01-22T19:45:03")
-            ),
-            Train(
-                arrayListOf("https://live.staticflickr.com/1745/41986168665_bbc48ba000_h.jpg"),
-                arrayListOf(
-                    TrainPart("2016", "59"),
-                    TrainPart("2016", "51"),
-                    TrainPart("21-73", "123"),
-                    TrainPart("21-73", "406"),
-                    TrainPart("21-73", "299"),
-                    TrainPart("80-73", "202")
-                ),
-                "Klagenfurt #2",
-                "REX3872",
-                "2x Hercules",
-                LocalDateTime.parse("2018-06-18T17:28:23")
-            )
-        );
-
         trains.clear()
         trainsAdapter.notifyDataSetChanged()
         DataBaseHelper.getTrainsReference()?.addListenerForSingleValueEvent(object : ValueEventListener {
